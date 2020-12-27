c3099f0550c9ebc5cce4d6d6cc32c8c52789a382
Jatin Sachdeva <60872915+Jatin6256@users.noreply.github.com>
2020-12-27 12:53:41 +0530

    send email even if grade of one course is changed

--- calculator.py
+++ calculator.py
@@ -12,6 +12,12 @@ msg = EmailMessage()
 
 url = 'https://aims.iiitr.ac.in/iiitraichur/courseReg/loadMyCoursesHistroy?studentId=3&courseCd=&courseName=&orderBy=1&degreeIds=&acadPeriodIds=&regTypeIds=&gradeIds=&resultIds=&isGradeIds='
 cookie = {'JSESSIONID' : <cookie_id>}
+#time interval = 600 secs 
+Time_Interval = 600
+acadPeriodId = <acad_period_id>
+sender_email = <sender_email>
+password = <password>
+send_to = <student_email>
 
 
 
@@ -41,8 +47,8 @@ def getPoints(grade):
 
     
 
-
-def getCgpa():
+prevCount = 0
+def getCgpa(cookie,acadPeriodId):
     r = requests.get(url, cookies=cookie)
     r = json.loads(r.content)
     points = 0
@@ -50,57 +56,91 @@ def getCgpa():
     grades = {}
     grades['name'] = []
     grades['grade'] = []
+    totalCourses = 0
+    currCount = 0
     for course in r:
-            if course['acadPeriodId'] == <acad_period_id>:
-                if course['gradeDesc'] == "" :
-                    grades['res'] = False
-                    grades['cgpa'] = 0
-                    return grades
-                else:
+            if course['acadPeriodId'] == acadPeriodId:
+                if course['gradeDesc'] != "" :
+                    currCount += 1
                     temp = (float(course['credits']) * getPoints(course['gradeDesc']))
                     points += temp 
                     totalCredits += float(course['credits'])
                     grades['grade'].append(course['gradeDesc'])
                     grades['name'].append(course['courseName'])
+                totalCourses += 1 
 
-   
-    if totalCredits != 0:
-        grades['res'] = True
+
+    global prevCount
+    if totalCourses == currCount :
+        grades['sendEmail'] = True
         grades['cgpa'] = points/totalCredits
+        grades['isFinal'] = True
+    elif currCount > prevCount :
+        prevCount = currCount
+        grades['cgpa'] = 0
+        grades['sendEmail'] = True
+        grades['isFinal'] = False
     else:
         grades['cgpa'] = 0
-        grades['res'] = False
+        grades['sendEmail'] = False
+        grades['isFinal'] = False
+        
     return grades
 
 def sendEmail():
-    dict = getCgpa()
-    if(dict['res'] == False):
-        time.sleep(600)
+    dict = getCgpa(cookie,acadPeriodId)
+    if(dict['sendEmail'] == False):
+        time.sleep(Time_Interval)
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print('[' + dt_string + ']: '+'Trying again')
         sendEmail()
-    else:
+    elif dict['isFinal'] == True:
         try:
             with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
 
-                smtp.login(<sender_email>, <password>)
+                smtp.login('sweprojectams@gmail.com', 'kvwnqvmrbftbhxiy')
 
                 msg["subject"] = "Semester Grades Uploaded"
-                msg["To"] = <student_email>
+                msg["To"] = 'cs19b1013@iiitr.ac.in'
                 msg["From"] = "AIMS Script <aims_script@testerworld.com>"
 
                 gradeList = ''
                 for i in range(len(dict['name'])):
                     gradeList += dict['name'][i] + ': ' + dict['grade'][i] + '\n'
 
-                print(gradeList)
                 msg.set_content("Your CGPA for the semester is " + str(dict['cgpa']) + ". Your break down of grades is:  \n\n" + gradeList )
 
                 smtp.send_message(msg)
                 print("Successfully sent email.")
         except SMTPException:
             print("Error: unable to send email")
+    else:
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+
+                smtp.login(sender_email, password)
+
+                msg["subject"] = "Semester Grades Uploaded"
+                msg["To"] = send_to
+                msg["From"] = "AIMS Script <aims_script@testerworld.com>"
+
+                gradeList = ''
+                for i in range(len(dict['name'])):
+                    gradeList += dict['name'][i] + ': ' + dict['grade'][i] + '\n'
+
+                msg.set_content("Your CGPA for the semester is not calculated yet. Your break down of grades is:  \n\n" + gradeList )
+
+                smtp.send_message(msg)
+                print("Successfully sent email.")
+        except SMTPException:
+            print("Error: unable to send email")
+    
+    time.sleep(Time_Interval)
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print('[' + dt_string + ']: '+'Trying again')
+    sendEmail()
 
 
 
